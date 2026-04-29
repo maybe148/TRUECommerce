2dd047aa20f6c5e99bb29074e0280fff981b4e83
Jerasak Sompreeda <maybe148@gmail.com>
2026-04-29 09:46:58 +0700

    Update product-configurator-feature.module.ts

--- js-storefront/spartacusstore/src/app/spartacus/features/product-configurator/product-configurator-feature.module.ts
+++ js-storefront/spartacusstore/src/app/spartacus/features/product-configurator/product-configurator-feature.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CmsConfig, I18nConfig, provideConfig } from "@spartacus/core";
 import { configuratorTranslationChunksConfig, configuratorTranslationsEn } from "@spartacus/product-configurator/common/assets";
-import { PRODUCT_CONFIGURATOR_RULEBASED_FEATURE, RulebasedConfiguratorRootModule } from "@spartacus/product-configurator/rulebased/root";
+//import { PRODUCT_CONFIGURATOR_RULEBASED_FEATURE, RulebasedConfiguratorRootModule } from "@spartacus/product-configurator/rulebased/root";
 import { PRODUCT_CONFIGURATOR_TEXTFIELD_FEATURE, TextfieldConfiguratorRootModule } from "@spartacus/product-configurator/textfield/root";
 
 @NgModule({
   declarations: [],
   imports: [
     TextfieldConfiguratorRootModule,
-    RulebasedConfiguratorRootModule
+    //RulebasedConfiguratorRootModule
   ],
   providers: [provideConfig(<CmsConfig>{
     featureModules: {
@@ -23,15 +23,15 @@ import { PRODUCT_CONFIGURATOR_TEXTFIELD_FEATURE, TextfieldConfiguratorRootModule
       resources: { en: configuratorTranslationsEn },
       chunks: configuratorTranslationChunksConfig,
     },
-  }),
-  provideConfig(<CmsConfig>{
-    featureModules: {
-      [PRODUCT_CONFIGURATOR_RULEBASED_FEATURE]: {
-        module: () =>
-          import('@spartacus/product-configurator/rulebased').then((m) => m.RulebasedConfiguratorModule),
-      },
-    }
   })
+  //provideConfig(<CmsConfig>{
+  //  featureModules: {
+  //    [PRODUCT_CONFIGURATOR_RULEBASED_FEATURE]: {
+  //      module: () =>
+  //        import('@spartacus/product-configurator/rulebased').then((m) => m.RulebasedConfiguratorModule),
+  //    },
+  //  }
+  //})
   ]
 })
 export class ProductConfiguratorFeatureModule { }
